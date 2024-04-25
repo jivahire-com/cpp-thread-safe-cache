bc80cd46beb2645001f1e783d96d55055723f93d
Kaushik S <kauss@microsoft.com>
2024-04-25 18:00:00 +0000

    Merged PR 71850: MSCP bootloader compilation folder structure
    ## Why is this change being made?
    The MSCP bootloader should be generated as a separate elf, this change adds the basic folders and cmake files needed to compile and generate the requisite elf and bin files.
    
    ## What is changed?
    Have added scp_boot_loader and mcp_boot_loader folders in src/apps along with the required cmake files. Upon building this will generate and elf and bin for the scp boot loader and mcp bootloader. These elf and bins are generated in addition to the current scp and mcp firmware files and doesn't impact them.
    
    The boot loader has the -Og flag enabled similar to how it is currently in Pioneer.
    
    ## How is the change being tested?
    
    - [ ] No test, document update only
    - [ ] Unit tests pass
    - [ ] Integration tests pass
    - [ ] Manual tests pass (add test specifics)
    - [x] Others, explain:
    
    This change is only the folders and confirm that building succeeds, currently the branch has been compiled using the cmake build command to confirm that scp_boot_loader.elf, scp_boot_loader.bin, mcp_boot_loader.elf and mcp_boot_loader.bin files are generated. The boot loader folders only have a single main file with just a main function returning zero and doesn't have any code implementation currently. These changes will added in future PRs.
    
    ### Test Platform
    
    - [ ] SVP
    - [ ] FPGA (Add FPGA Config)
    - [ ] Silicon
    - [x] Others, explain:
    Only compilation is tested on devbox
    
    ## Notes
    This is the first PR in generation of the boot loader, upcoming changes will add the files needed for decompression, proper logic to get load address and functions to switch and execute from ITCM.
    
    ## Risks
    
    Related work items: #1484339

---
+++ src/apps/scp_boot_loader/src/main.c
@@ -0,0 +1,25 @@
+#include <stdint.h>
+
+/*
+ * The SCP boot loader is expected to perform the following actions:
+ * 1. Hold the compressed SCP firmware in mainimage section of boot binary
+ * 2. Uncompress the SCP firmware into SCP ITCM and DTCM based on the sections
+ * 3. Jump to ITCM and execute the SCP firmware.
+ */
+
+/* TODO: Create a placeholder variable for the mainimage section - the SCP firmware will be copied into this section
+ * ADO: https://azurecsi.visualstudio.com/Dev/_workitems/edit/1484340
+*/
+
+/* TODO: ADO: Invoke APIs to uncompress the firmware in the mainimage section
+*  ADO: https://azurecsi.visualstudio.com/Dev/_workitems/edit/1484340
+*/
+
+/* TODO: ADO: Update program counter to point to ITCM and start executing the SCP firmware
+*  ADO: https://azurecsi.visualstudio.com/Dev/_workitems/edit/1484340
+*/
+
+int main(void)
+{
+    return 0;
+}

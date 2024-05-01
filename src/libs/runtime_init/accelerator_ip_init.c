c4d1b62a656a14b6e7ea8038744f7236ad184c41
Sunny Bhayani <sunnybhayani@microsoft.com>
2024-05-01 15:12:31 +0000

    Merged PR 72237: runtime: accel: update the FPFW Init Dependencies
    ## Why is this change being made?
    The Accelerator IP library depends on `std_io`, `hw_ver` and `mesh` as FPFW Init Dependencies.
    
    Because of this, in this [init.c](https://azurecsi.visualstudio.com/DuvallFw/_git/1pfw.fwlibs?path=%2Fmodules%2Flibs%2FInit%2Finit.c&_a=contents&version=GBmain) -> `fpfw_init()`, the `s_init_context.head` is `NULL` at [this line](https://azurecsi.visualstudio.com/DuvallFw/_git/1pfw.fwlibs?path=/modules/libs/Init/init.c&version=GBmain&line=61&lineEnd=62&lineStartColumn=1&lineEndColumn=1&lineStyle=plain&_a=contents).
    
    This is a bug which should be fixed which is captured in the below ADO:
    https://azurecsi.visualstudio.com/Woodinville/_workitems/edit/1775434
    
    ## What is changed?
    Update the FPFW Init Dependencies for the Accelerator IP library on `css_pome`.
    
    This `css_pome` includes the other dependencies needed by accelerator IP library i.e. `std_io`, `hw_ver` and `mesh`.
    
    ## How is the change being tested?
    
    - [ ] No test, document update only
    - [ ] Unit tests pass
    - [ ] Integration tests pass
    - [x] Manual tests pass (add test specifics): Confirmed that the FPFW Init list is non-NULL via T32
    - [ ] Others, explain:
    
    ### Test Platform
    
    - [ ] SVP
    - [x] FPGA (Add FPGA Config): R13
    - [ ] Silicon
    - [ ] Others, explain:
    
    ## Notes
    
    ## Risks
    
    Related work items: #1710832

--- src/libs/runtime_init/accelerator_ip_init.c
+++ src/libs/runtime_init/accelerator_ip_init.c
@@ -15,7 +15,15 @@
 
 /*------------- Functions ----------------*/
 
-FPFW_INIT_COMPONENT(accel, FPFW_INIT_DEPENDENCIES("std_io", "hw_ver", "mesh"))
+/*
+ * TODO: ADO: 1775434
+ *
+ * Enabling the explicit dependency list is causing the `s_init_context.head` in
+ * the `fpfw_init()` path as `NULL`. Hence as a fix, we are just calling out the
+ * `css_pome` which in-turn depends on the `std_io`, `hw_ver` and `mesh`.
+ */
+//FPFW_INIT_COMPONENT(accel, FPFW_INIT_DEPENDENCIES("std_io", "hw_ver", "mesh"))
+FPFW_INIT_COMPONENT(accel, FPFW_INIT_DEPENDENCIES("css_pome"))
 {
     // Initialize the Accelerators
     printf("Accelerator init start!!!\n");
@@ -24,3 +32,4 @@ FPFW_INIT_COMPONENT(accel, FPFW_INIT_DEPENDENCIES("std_io", "hw_ver", "mesh"))
 
     return (fpfw_init_result_t){FPFW_INIT_STATUS_SUCCESS, NULL};
 }
+

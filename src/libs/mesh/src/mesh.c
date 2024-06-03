d9aa09604ea02610e922237ac9842338a81bd57c
Aditya Tantry <adityatantry@microsoft.com>
2024-06-03 22:45:41 +0000

    Merged PR 76142: Mesh Dual Die Support
    ## Why is this change being made?
    Add support for Mesh dual die on the Prod FW for KNG
    ## What is changed?
    Update the Mesh Lib and the Unit Tests to support Dual die boot
    ## How is the change being tested?
    
    - [ ] No test, document update only
    - [x] Unit tests pass
    - [ ] Integration tests pass
    - [ ] Manual tests pass (add test specifics)
    - [ ] Others, explain:
    
    ### Test Platform
    
    - [x] SVP
    - [ ] FPGA (Add FPGA Config)
    - [ ] Silicon
    - [ ] Others, explain:
    
    ## Notes
    
    ## Risks
    
    Related work items: #1831678

--- src/libs/mesh/src/mesh.c
+++ src/libs/mesh/src/mesh.c
@@ -8,14 +8,19 @@
  */
 
 /*------------- Includes -----------------*/
-#include <FpFwAssert.h>
-#include <cmn800_sequence.h>
-#include <cmn_config.h>
-#include <idsw.h>
-#include <kng_soc_constants.h>
-#include <mesh.h>
-#include <stdint.h>
-#include <stdio.h>
+#include <FpFwAssert.h>        // for FPFW_RUNTIME_ASSERT
+#include <cmn800_sequence.h>   // for cmn800_sequence, d2dss_sequence, cmn8...
+#include <cmn_config.h>        // for CMN800_CONFIG_CONFIG
+#include <idhw.h>              // for idhw_is_single_die_boot_en
+#include <idsw.h>              // for idsw_get_platform_sdv, _PLAT_ID
+#include <kng_soc_constants.h> // for NUM_DIE
+#include <mesh.h>              // for mesh_init
+#include <stdbool.h>           // for true
+#include <stdint.h>            // for uint8_t
+#include <stdio.h>             // for printf
+
+/*------------- Defines -----------------*/
+bool dual_die_boot = false; // ADO: 1825901 Deprecate after ADO is implemented by SVP
 
 /*------------- Functions ----------------*/
 
@@ -28,19 +33,40 @@ void mesh_init(uint8_t die_num)
     cmn800_sequence_params_t cmn800_sequence_param = {};
     cmn800_sequence_param.die_num = die_num;
     cmn800_sequence_param.cmn_config_enum = CONFIG_1D_UMA_64HNS_HIER_3SN_enum;
-    cmn800_sequence_param.CMN_INIT = 2;
+    cmn800_sequence_param.CMN_INIT = 2; // CMN_INIT_FRONTDOOR_HNS_PPU_POLL_CMN_SAM_CFG
     cmn800_sequence_param.DRAM_SIZE = DRAM_32GB;
-    cmn800_sequence_param.HNS_SPARE_DIE0 = 0;
-    cmn800_sequence_param.HNS_SPARE_DIE1 = 0;
-    cmn800_sequence_param.D2D_INIT = 0;
     cmn800_sequence_param.BOOT_2D_ENABLE = 0;
+    cmn800_sequence_param.HNS_SPARE_DIE0 = 0; // CMN_HNS_SPARE_CONFIG0_X_X_X_X
+    cmn800_sequence_param.HNS_SPARE_DIE1 = 0; // CMN_HNS_SPARE_CONFIG0_X_X_X_X
+    cmn800_sequence_param.D2D_INIT = 0;       // D2D_INIT_FRONTDOOR
     cmn800_sequence_param.USE_HW_TIMER = 0;
 
+    if (dual_die_boot == true) // ADO: 1825901 Deprecate after ADO is implemented by SVP
+    {
+        if (!idhw_is_single_die_boot_en()) // 2 Die
+        {
+            printf("Dual Die Boot\n");
+            cmn800_sequence_param.cmn_config_enum = CONFIG_2D_NUMA_64HNS_HIER_3SN_enum; // 2 Die
+            cmn800_sequence_param.BOOT_2D_ENABLE = true;
+        }
+    }
     if (idsw_get_platform_sdv() == PLATFORM_FPGA_LARGE)
     {
-        cmn800_sequence_param.cmn_config_enum = CONFIG_1D_UMA_8HNS_HIER_3SN_enum; // 1 Die FPGA
+        if (idhw_is_single_die_boot_en())
+        {
+            cmn800_sequence_param.cmn_config_enum = CONFIG_1D_UMA_8HNS_HIER_3SN_enum; // 1 Die FPGA
+        }
+        else
+        {
+            cmn800_sequence_param.cmn_config_enum = CONFIG_2D_NUMA_8HNS_HIER_3SN_enum; // 2 Die FPGA
+            cmn800_sequence_param.BOOT_2D_ENABLE = true;
+        }
     }
     printf("cmn800_sequence_param.cmn_config_enum 0x%x\n", (uint8_t)cmn800_sequence_param.cmn_config_enum);
+
+    sts = cmn800_sequence_svp_updates(cmn800_sequence_param);
+    printf("cmn800_sequence_svp_updates sts 0x%x\n", sts);
+    FPFW_RUNTIME_ASSERT(sts == 0);
 
     sts = cmn800_sequence(cmn800_sequence_param);
     printf("cmn800_sequence sts 0x%x\n", sts);
--- tests/unit/libs/mesh/test_mesh.cpp
+++ tests/unit/libs/mesh/test_mesh.cpp
@@ -24,6 +24,8 @@ extern "C" {
 /*-------- Function Prototypes -----------*/
 
 /*-- Declarations (Statics and globals) --*/
+bool simulate_single_die = true;
+extern bool dual_die_boot; // ADO: 1825901 Deprecate after ADO is implemented by SVP
 
 /*------------- Functions ----------------*/
 //
@@ -33,6 +35,16 @@ static int setup_svp_platform(void** pContext)
 {
     FPFW_UNUSED(pContext);
     idsw_set_platform_sdv(PLATFORM_SVP_SIM);
+    simulate_single_die = true;
+    return 0;
+}
+
+static int setup_svp_platform_dual_die(void** pContext)
+{
+    FPFW_UNUSED(pContext);
+    idsw_set_platform_sdv(PLATFORM_SVP_SIM);
+    simulate_single_die = false;
+    dual_die_boot = true; // ADO: 1825901 Deprecate after ADO is implemented by SVP
     return 0;
 }
 
@@ -40,6 +52,15 @@ static int setup_fpga_platform(void** pContext)
 {
     FPFW_UNUSED(pContext);
     idsw_set_platform_sdv(PLATFORM_FPGA_LARGE);
+    simulate_single_die = true;
+    return 0;
+}
+
+static int setup_fpga_platform_dual_die(void** pContext)
+{
+    FPFW_UNUSED(pContext);
+    idsw_set_platform_sdv(PLATFORM_FPGA_LARGE);
+    simulate_single_die = false;
     return 0;
 }
 
@@ -47,6 +68,7 @@ static int setup_undefined_platform(void** pContext)
 {
     FPFW_UNUSED(pContext);
     idsw_set_platform_sdv(PLATFORM_UNDEFINED);
+    simulate_single_die = true;
     return 0;
 }
 
@@ -66,6 +88,31 @@ int __wrap_cmn800_sequence(cmn800_sequence_params_t cmn800_sequence_param)
     return 0;
 }
 
+int __wrap_d2dss_sequence(cmn800_sequence_params_t cmn800_sequence_param)
+{
+    check_expected(cmn800_sequence_param.die_num);
+    check_expected(cmn800_sequence_param.cmn_config_enum);
+    check_expected(cmn800_sequence_param.CMN_INIT);
+    check_expected(cmn800_sequence_param.BOOT_2D_ENABLE);
+    check_expected(cmn800_sequence_param.HNS_SPARE_DIE0);
+    check_expected(cmn800_sequence_param.HNS_SPARE_DIE1);
+    check_expected(cmn800_sequence_param.D2D_INIT);
+    function_called();
+    return 0;
+}
+
+int __wrap_cmn800_sequence_svp_updates(cmn800_sequence_params_t cmn800_sequence_param)
+{
+    check_expected(cmn800_sequence_param.die_num);
+
+    function_called();
+    return 0;
+}
+bool __wrap_idhw_is_single_die_boot_en(void)
+{
+    return simulate_single_die;
+}
+
 //
 // Tests
 //
@@ -74,6 +121,8 @@ TEST_FUNCTION(test_mesh_init_single_die_boot_Die_0_SVP, setup_svp_platform, setu
 {
     // Set up expectations
     const auto test_die = (DIE_ID)0;
+    expect_value(__wrap_cmn800_sequence_svp_updates, cmn800_sequence_param.die_num, test_die);
+    expect_function_call(__wrap_cmn800_sequence_svp_updates);
 
     expect_value(__wrap_cmn800_sequence, cmn800_sequence_param.die_num, test_die);
     expect_value(__wrap_cmn800_sequence, cmn800_sequence_param.cmn_config_enum, CONFIG_1D_UMA_64HNS_HIER_3SN_enum);
@@ -92,6 +141,10 @@ TEST_FUNCTION(test_mesh_init_single_die_boot_Die_1_SVP, setup_svp_platform, setu
 {
     // Set up expectations
     const auto test_die = (DIE_ID)1;
+
+    expect_value(__wrap_cmn800_sequence_svp_updates, cmn800_sequence_param.die_num, test_die);
+    expect_function_call(__wrap_cmn800_sequence_svp_updates);
+
     expect_value(__wrap_cmn800_sequence, cmn800_sequence_param.die_num, test_die);
     expect_value(__wrap_cmn800_sequence, cmn800_sequence_param.cmn_config_enum, CONFIG_1D_UMA_64HNS_HIER_3SN_enum);
     expect_value(__wrap_cmn800_sequence, cmn800_sequence_param.CMN_INIT, 2);
@@ -109,6 +162,10 @@ TEST_FUNCTION(test_mesh_init_single_die_boot_Die_0_FPGA, setup_fpga_platform, se
 {
     // Set up expectations
     const auto test_die = (DIE_ID)0;
+
+    expect_value(__wrap_cmn800_sequence_svp_updates, cmn800_sequence_param.die_num, test_die);
+    expect_function_call(__wrap_cmn800_sequence_svp_updates);
+
     expect_value(__wrap_cmn800_sequence, cmn800_sequence_param.die_num, test_die);
     expect_value(__wrap_cmn800_sequence, cmn800_sequence_param.cmn_config_enum, CONFIG_1D_UMA_8HNS_HIER_3SN_enum);
     expect_value(__wrap_cmn800_sequence, cmn800_sequence_param.CMN_INIT, 2);
@@ -126,6 +183,10 @@ TEST_FUNCTION(test_mesh_init_single_die_boot_Die_1_FPGA, setup_fpga_platform, se
 {
     // Set up expectations
     const auto test_die = (DIE_ID)1;
+
+    expect_value(__wrap_cmn800_sequence_svp_updates, cmn800_sequence_param.die_num, test_die);
+    expect_function_call(__wrap_cmn800_sequence_svp_updates);
+
     expect_value(__wrap_cmn800_sequence, cmn800_sequence_param.die_num, test_die);
     expect_value(__wrap_cmn800_sequence, cmn800_sequence_param.cmn_config_enum, CONFIG_1D_UMA_8HNS_HIER_3SN_enum);
     expect_value(__wrap_cmn800_sequence, cmn800_sequence_param.CMN_INIT, 2);
@@ -134,6 +195,126 @@ TEST_FUNCTION(test_mesh_init_single_die_boot_Die_1_FPGA, setup_fpga_platform, se
     expect_value(__wrap_cmn800_sequence, cmn800_sequence_param.HNS_SPARE_DIE1, 0);
     expect_value(__wrap_cmn800_sequence, cmn800_sequence_param.D2D_INIT, 0);
     expect_function_call(__wrap_cmn800_sequence);
+
+    // Call API under test
+    mesh_init(test_die);
+}
+
+// Dual Die Boot
+TEST_FUNCTION(test_mesh_init_dual_die_boot_Die_0_SVP, setup_svp_platform_dual_die, setup_undefined_platform)
+{
+    // Set up expectations
+    const auto test_die = (DIE_ID)0;
+
+    expect_value(__wrap_cmn800_sequence_svp_updates, cmn800_sequence_param.die_num, test_die);
+    expect_function_call(__wrap_cmn800_sequence_svp_updates);
+
+    expect_value(__wrap_cmn800_sequence, cmn800_sequence_param.die_num, test_die);
+    expect_value(__wrap_cmn800_sequence, cmn800_sequence_param.cmn_config_enum, CONFIG_2D_NUMA_64HNS_HIER_3SN_enum);
+    expect_value(__wrap_cmn800_sequence, cmn800_sequence_param.CMN_INIT, 2);
+    expect_value(__wrap_cmn800_sequence, cmn800_sequence_param.BOOT_2D_ENABLE, true);
+    expect_value(__wrap_cmn800_sequence, cmn800_sequence_param.HNS_SPARE_DIE0, 0);
+    expect_value(__wrap_cmn800_sequence, cmn800_sequence_param.HNS_SPARE_DIE1, 0);
+    expect_value(__wrap_cmn800_sequence, cmn800_sequence_param.D2D_INIT, 0);
+    expect_function_call(__wrap_cmn800_sequence);
+
+    expect_value(__wrap_d2dss_sequence, cmn800_sequence_param.die_num, test_die);
+    expect_value(__wrap_d2dss_sequence, cmn800_sequence_param.cmn_config_enum, CONFIG_2D_NUMA_64HNS_HIER_3SN_enum);
+    expect_value(__wrap_d2dss_sequence, cmn800_sequence_param.CMN_INIT, 2);
+    expect_value(__wrap_d2dss_sequence, cmn800_sequence_param.BOOT_2D_ENABLE, true);
+    expect_value(__wrap_d2dss_sequence, cmn800_sequence_param.HNS_SPARE_DIE0, 0);
+    expect_value(__wrap_d2dss_sequence, cmn800_sequence_param.HNS_SPARE_DIE1, 0);
+    expect_value(__wrap_d2dss_sequence, cmn800_sequence_param.D2D_INIT, 0);
+    expect_function_call(__wrap_d2dss_sequence);
+
+    // Call API under test
+    mesh_init(test_die);
+}
+
+TEST_FUNCTION(test_mesh_init_dual_die_boot_Die_1_SVP, setup_svp_platform_dual_die, setup_undefined_platform)
+{
+    // Set up expectations
+    const auto test_die = (DIE_ID)1;
+
+    expect_value(__wrap_cmn800_sequence_svp_updates, cmn800_sequence_param.die_num, test_die);
+    expect_function_call(__wrap_cmn800_sequence_svp_updates);
+
+    expect_value(__wrap_cmn800_sequence, cmn800_sequence_param.die_num, test_die);
+    expect_value(__wrap_cmn800_sequence, cmn800_sequence_param.cmn_config_enum, CONFIG_2D_NUMA_64HNS_HIER_3SN_enum);
+    expect_value(__wrap_cmn800_sequence, cmn800_sequence_param.CMN_INIT, 2);
+    expect_value(__wrap_cmn800_sequence, cmn800_sequence_param.BOOT_2D_ENABLE, true);
+    expect_value(__wrap_cmn800_sequence, cmn800_sequence_param.HNS_SPARE_DIE0, 0);
+    expect_value(__wrap_cmn800_sequence, cmn800_sequence_param.HNS_SPARE_DIE1, 0);
+    expect_value(__wrap_cmn800_sequence, cmn800_sequence_param.D2D_INIT, 0);
+    expect_function_call(__wrap_cmn800_sequence);
+
+    expect_value(__wrap_d2dss_sequence, cmn800_sequence_param.die_num, test_die);
+    expect_value(__wrap_d2dss_sequence, cmn800_sequence_param.cmn_config_enum, CONFIG_2D_NUMA_64HNS_HIER_3SN_enum);
+    expect_value(__wrap_d2dss_sequence, cmn800_sequence_param.CMN_INIT, 2);
+    expect_value(__wrap_d2dss_sequence, cmn800_sequence_param.BOOT_2D_ENABLE, true);
+    expect_value(__wrap_d2dss_sequence, cmn800_sequence_param.HNS_SPARE_DIE0, 0);
+    expect_value(__wrap_d2dss_sequence, cmn800_sequence_param.HNS_SPARE_DIE1, 0);
+    expect_value(__wrap_d2dss_sequence, cmn800_sequence_param.D2D_INIT, 0);
+    expect_function_call(__wrap_d2dss_sequence);
+
+    // Call API under test
+    mesh_init(test_die);
+}
+
+TEST_FUNCTION(test_mesh_init_dual_die_boot_Die_0_FPGA, setup_fpga_platform_dual_die, setup_undefined_platform)
+{
+    // Set up expectations
+    const auto test_die = (DIE_ID)0;
+
+    expect_value(__wrap_cmn800_sequence_svp_updates, cmn800_sequence_param.die_num, test_die);
+    expect_function_call(__wrap_cmn800_sequence_svp_updates);
+
+    expect_value(__wrap_cmn800_sequence, cmn800_sequence_param.die_num, test_die);
+    expect_value(__wrap_cmn800_sequence, cmn800_sequence_param.cmn_config_enum, CONFIG_2D_NUMA_8HNS_HIER_3SN_enum);
+    expect_value(__wrap_cmn800_sequence, cmn800_sequence_param.CMN_INIT, 2);
+    expect_value(__wrap_cmn800_sequence, cmn800_sequence_param.BOOT_2D_ENABLE, true);
+    expect_value(__wrap_cmn800_sequence, cmn800_sequence_param.HNS_SPARE_DIE0, 0);
+    expect_value(__wrap_cmn800_sequence, cmn800_sequence_param.HNS_SPARE_DIE1, 0);
+    expect_value(__wrap_cmn800_sequence, cmn800_sequence_param.D2D_INIT, 0);
+    expect_function_call(__wrap_cmn800_sequence);
+
+    expect_value(__wrap_d2dss_sequence, cmn800_sequence_param.die_num, test_die);
+    expect_value(__wrap_d2dss_sequence, cmn800_sequence_param.cmn_config_enum, CONFIG_2D_NUMA_8HNS_HIER_3SN_enum);
+    expect_value(__wrap_d2dss_sequence, cmn800_sequence_param.CMN_INIT, 2);
+    expect_value(__wrap_d2dss_sequence, cmn800_sequence_param.BOOT_2D_ENABLE, true);
+    expect_value(__wrap_d2dss_sequence, cmn800_sequence_param.HNS_SPARE_DIE0, 0);
+    expect_value(__wrap_d2dss_sequence, cmn800_sequence_param.HNS_SPARE_DIE1, 0);
+    expect_value(__wrap_d2dss_sequence, cmn800_sequence_param.D2D_INIT, 0);
+    expect_function_call(__wrap_d2dss_sequence);
+
+    // Call API under test
+    mesh_init(test_die);
+}
+
+TEST_FUNCTION(test_mesh_init_dual_die_boot_Die_1_FPGA, setup_fpga_platform_dual_die, setup_undefined_platform)
+{
+    // Set up expectations
+    const auto test_die = (DIE_ID)1;
+    expect_value(__wrap_cmn800_sequence_svp_updates, cmn800_sequence_param.die_num, test_die);
+    expect_function_call(__wrap_cmn800_sequence_svp_updates);
+
+    expect_value(__wrap_cmn800_sequence, cmn800_sequence_param.die_num, test_die);
+    expect_value(__wrap_cmn800_sequence, cmn800_sequence_param.cmn_config_enum, CONFIG_2D_NUMA_8HNS_HIER_3SN_enum);
+    expect_value(__wrap_cmn800_sequence, cmn800_sequence_param.CMN_INIT, 2);
+    expect_value(__wrap_cmn800_sequence, cmn800_sequence_param.BOOT_2D_ENABLE, true);
+    expect_value(__wrap_cmn800_sequence, cmn800_sequence_param.HNS_SPARE_DIE0, 0);
+    expect_value(__wrap_cmn800_sequence, cmn800_sequence_param.HNS_SPARE_DIE1, 0);
+    expect_value(__wrap_cmn800_sequence, cmn800_sequence_param.D2D_INIT, 0);
+    expect_function_call(__wrap_cmn800_sequence);
+
+    expect_value(__wrap_d2dss_sequence, cmn800_sequence_param.die_num, test_die);
+    expect_value(__wrap_d2dss_sequence, cmn800_sequence_param.cmn_config_enum, CONFIG_2D_NUMA_8HNS_HIER_3SN_enum);
+    expect_value(__wrap_d2dss_sequence, cmn800_sequence_param.CMN_INIT, 2);
+    expect_value(__wrap_d2dss_sequence, cmn800_sequence_param.BOOT_2D_ENABLE, true);
+    expect_value(__wrap_d2dss_sequence, cmn800_sequence_param.HNS_SPARE_DIE0, 0);
+    expect_value(__wrap_d2dss_sequence, cmn800_sequence_param.HNS_SPARE_DIE1, 0);
+    expect_value(__wrap_d2dss_sequence, cmn800_sequence_param.D2D_INIT, 0);
+    expect_function_call(__wrap_d2dss_sequence);
 
     // Call API under test
     mesh_init(test_die);

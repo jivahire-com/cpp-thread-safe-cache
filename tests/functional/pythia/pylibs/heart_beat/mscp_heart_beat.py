3ef496ecbd85439e01453de35629dfbf668531fe
Parth Davé <parthdave@microsoft.com>
2024-09-10 13:26:46 +0000

    Merged PR 86304: Remove quotes in SVP config, update integ test to use .dat file
    ## Why is this change being made?
    
    Remove quotes in SVP config, make sure .dat boot is healthy
    
    ## What is changed?
    
    Remove quotes in SVP config, update boot integ test to use .dat file
    
    ## How is the change being tested?
    
    - [ ] No test, document update only
    - [ ] Unit tests pass
    - [x] Integration tests pass
    - [x] Manual tests pass (add test specifics)
    - [ ] Others, explain:
    
    ### Test Platform
    
    - [x] SVP
    - [ ] FPGA (Add FPGA Config)
    - [ ] Silicon
    - [ ] Others, explain:
    
    ----
    #### AI description  (iteration 1)
    #### PR Classification
    Code cleanup to remove unnecessary quotes from configuration values.
    
    #### PR Summary
    This pull request removes quotes from boolean and string values in a configuration file to ensure consistency and correctness.
    - Changes in `/tools/vpcfg/svpcfg-hsp_bl_scp.txt` to remove quotes from `runtime_disabled` and `disabled_behavior` properties.
    
    Related work items: #2020880

---
+++ tests/functional/pythia/pylibs/heart_beat/mscp_heart_beat.py
@@ -0,0 +1,114 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+
+"""
+- Python based Pythia 2.0 Test.
+- Tests the HeartBeat from MSCP via UART.
+"""
+import time
+import sys, os
+from pathlib import Path
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'kng_pythia_libs'))
+
+from kng_pythia_test_if import KngPythiaTestIF
+from kng_pythia_test_setup import KngPythiaTestSetup
+
+from pythia.tdk.echofalls.constants.dut_types import DeviceType
+
+from pythia.tdk.echofalls.echofalls_base_test import EchoFallsBaseTest
+
+# Class name must match file name for Robot Framework Library usage
+class mscp_heart_beat(EchoFallsBaseTest):
+
+    """
+    :param name:                Name of the test case
+    :param number:              ADO Number of the test case
+    :param workspace_config:    Path to the workspace config file
+    :param default_log_home:    Path for log storage
+    :param fw_payload_path:     Path to the recipe payload
+    :param dut_platform:        Platform used during the test case execution
+    :param host_config:         Path to the host config file/directory
+    :param host_name:           Name of the host to find the host config file, if host_config is a directory. Defaults to None
+    """
+    def __init__(
+        self,
+        name: str = None,
+        number: str = "NaN",
+        workspace_config: Path | str = None,
+        default_log_home: str = None,
+        fw_payload_path: str = None,
+        dut_platform: str = "KingsgateSVP",
+        host_config: Path | str = None,
+        host_name: str | None = None,
+    ):
+        
+        # Call parent class init
+        super().__init__(
+            name,
+            number,
+            workspace_config,
+            default_log_home,
+            fw_payload_path,
+            dut_platform,
+            host_config,
+            host_name,
+        )
+    
+    def mscp_heartbeat(self):
+        """
+        MSCP heartbeat test:
+            1. Setup the Test.
+            2. Look for the SCP HeartBeat.
+            2. Look for the MCP HeartBeat.
+            3. Teardown Test.
+        """
+        self.log.info("Running MSCP heartBeat test . . .")
+        self.dut.setup()
+
+        if (self.dut.get_dut_type() == DeviceType.BIGFPGA):
+            KngPythiaTestSetup.reset_fpga_load_prodfw(self)
+            time.sleep(30)
+        
+        elif (self.dut.get_dut_type() == DeviceType.SVP):
+            # Ensure the host config file used alongside this test has these connections defined.
+            assert self.dut.mb.node_0.soc.primary_die.scp.channel_manager is not None
+            self.dut.mb.node_0.soc.primary_die.scp.channel_manager.get_current_channel().open()
+            assert self.dut.mb.node_0.soc.primary_die.scp.channel_manager.get_current_channel().is_open()
+
+            assert self.dut.mb.node_0.soc.primary_die.mcp.channel_manager is not None
+            self.dut.mb.node_0.soc.primary_die.mcp.channel_manager.get_current_channel().open()
+            assert self.dut.mb.node_0.soc.primary_die.mcp.channel_manager.get_current_channel().is_open()
+
+        else:
+            self.log.error("Unsupported DUT type")
+            self.dut.teardown()
+            return False
+
+
+        self.log.info("Reading SCP UART for HeartBeat . . .")
+        try:
+            connection = self.dut.mb.node_0.soc.primary_die.scp.channel_manager
+            connection.get_current_channel().read_until(key="HeartBeat", timeout_seconds=120)
+        except Exception as e:
+            self.log.error(f"Error reading SCP UART: {e}")
+            self.dut.mb.node_0.soc.primary_die.scp.channel_manager.get_current_channel().close()
+            self.test_notify(step="HeartBeat", msg="Test Fail", _is_error=True)
+            self.dut.teardown()
+            return False
+
+        self.log.info("Reading MCP UART for HeartBeat . . .")
+        try:
+            connection = self.dut.mb.node_0.soc.primary_die.mcp.channel_manager
+            connection.get_current_channel().read_until(key="HeartBeat", timeout_seconds=120)
+        except Exception as e:
+            self.log.error(f"Error reading MCP UART: {e}")
+            self.dut.mb.node_0.soc.primary_die.scp.channel_manager.get_current_channel().close()
+            self.test_notify(step="HeartBeat", msg="Test Fail", _is_error=True)
+            self.dut.teardown()
+            return False
+
+        self.dut.mb.node_0.soc.primary_die.scp.channel_manager.get_current_channel().close()
+        self.test_notify(step="HeartBeat", msg="Test Done", _is_error=False)
+        self.dut.teardown()
+
+        return True

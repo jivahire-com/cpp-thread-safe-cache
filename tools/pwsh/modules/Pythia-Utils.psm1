5e950a2570b7b734dea9e78ffdc424bb56545282
Yokesh Maradona Kagoo Bertus Kagoo <v-ybertuskag@microsoft.com>
2024-09-18 02:41:30 +0000

    Merged PR 86489: SVP_Weekend_Stress Pipeline
    ## Why is this change being made?
    To introduce MSCP weekend stress pipeline on SVP platform
    ## What is changed?
    New pipeline YAML file has been deployed
    ## How is the change being tested?
    
    - [ ] No test, document update only
    - [ ] Unit tests pass
    - [ ] Integration tests pass
    - [x] Manual tests pass (add test specifics)
    - [ ] Others, explain:
    https://azurecsi.visualstudio.com/Woodinville/_build/results?buildId=770452&view=results
    ### Test Platform
    
    - [x] SVP
    - [ ] FPGA (Add FPGA Config)
    - [ ] Silicon
    - [ ] Others, explain:
    
    ## Notes
    [Kingsgate_Pipeline (2).docx](https://azurecsi.visualstudio.com/2610da6a-0168-48f8-b34a-f1c882cb6660/_apis/git/repositories/8fc5d0fc-7430-45b1-8963-5d075fba03da/pullRequests/86489/attachments/Kingsgate_Pipeline%20%282%29.docx)
    
    ## Risks
    
    ----
    #### AI description  (iteration 1)
    #### PR Classification
    New feature: Implementation of nightly/weekly test pipelines and dashboards.
    
    #### PR Summary
    This pull request introduces a new stress test pipeline for SVP, including the ability to run multiple iterations of tests and generate detailed logs.
    - `tools/pwsh/modules/Pythia-Utils.psm1`: Added `Invoke-Pythia-Stress` function to handle stress test iterations and log results.
    - `tools/pipelines/svp_weekend_stress.yaml`: Created a new pipeline configuration for weekend stress tests.
    - `tools/pipelines/templates/svp_pythia_test.yaml`: Updated to include stress test iterations parameter and conditional invocation of `Invoke-Pythia-Stress`.
    
    Related work items: #1917163

--- tools/pwsh/modules/Pythia-Utils.psm1
+++ tools/pwsh/modules/Pythia-Utils.psm1
@@ -141,3 +141,122 @@ Function Invoke-Pythia(
     }
 
 }
+<#
+.SYNOPSIS
+Starts a local Pythia Test Run, using the SVP Recipe.
+
+.PARAMETER test
+Path to the test to run. Can be a robot file, or a directory containing robot files.
+
+.EXAMPLE
+Invoke-Pythia-Stress -test .\tests\functional\pythia\test_suites\heart_beat\
+#>
+Function Invoke-Pythia-Stress(
+    [Parameter(Mandatory=$true)] [string] $test,
+    [Parameter(Mandatory=$true)] [ValidateSet('svp', 'fpga')] [string] $platform = "svp",
+    [Parameter(Mandatory=$true)] [int] $stress_iteration
+)
+{
+
+    Write-Host ""
+    Write-Host "`tInvoking Pythia Functional Test with iteration $stress_iteration"
+    Write-Host ""
+
+    $test = Join-Path -Path $env:REPO_APP_ROOT -ChildPath $test
+
+    if (-not (Test-Path -Path $test))
+    {
+        throw "Path not found: $test"
+    }
+    # Loop through the stress iterations
+    for ($i = 1; $i -le $stress_iteration; $i++) 
+    {
+        Write-Host ""
+        Write-Host "`tStarting iteration $i of $stress_iteration"
+        Write-Host ""        
+
+    # Create test run paths based on timestamp
+    $time_stamp = (Get-Date -Format "yyyy_MM_dd_hhmmss")
+    $pythia_test_dir = Join-Path -Path $env:REPO_APP_TEST_LOG_DIR -ChildPath "pythia"
+    $test_results_dir = Join-Path -Path $pythia_test_dir -ChildPath "$time_stamp-iteration-$i"
+
+    # Create the test results directory
+    New-Item -ItemType Directory -Path $test_results_dir | Out-Null
+
+    # Setup the paths for the necessary Pythia configurations
+    # In\Out for pre/post processed json files (for variable expansion)
+
+    $workspace_config_in = Join-Path -Path $env:REPO_APP_ROOT -ChildPath "tests\functional\pythia\configs\svp_workspace.json"
+    $workspace_config_out = Join-Path -Path $test_results_dir -ChildPath "workspace.json"
+
+    # Create new json files with expanded string values, expanding environment variables
+    Expand-File -in_file $workspace_config_in -out_file $workspace_config_out
+
+    if ($platform -eq "svp") {
+        $host_configs_path = Join-Path -Path $env:REPO_APP_ROOT -ChildPath "tests\functional\pythia\hosts\svp_hosts"
+    } elseif ($platform -eq "fpga") {
+        $host_configs_path = Join-Path -Path $env:REPO_APP_ROOT -ChildPath "tests\functional\pythia\hosts\fpga_hosts"
+    }
+
+    $iteration = 1
+    Get-ChildItem $host_configs_path |
+    Foreach-Object {
+        $host_config_in = Join-Path $host_configs_path -ChildPath $_.Name
+        $host_config_out = Join-Path -Path $test_results_dir -ChildPath $_.Name
+
+        # Create new json files with expanded string values, expanding environment variables
+        Expand-File -in_file $host_config_in -out_file $host_config_out
+        Write-Host "`t` Host Config $iteration : $host_config_out"
+        $iteration++
+    }
+
+    # Set the payload to the pre-made SVP Payload
+    $recipe_payload_dir = Join-Path -Path $env:REPO_APP_ROOT -ChildPath "tests\functional\pythia\payloads\payload_svp"
+
+    Write-Host ""
+    Write-Host "`tStoring Test Results: $test_results_dir "
+    Write-Host ""
+
+    Write-Host ""
+    Write-Host "`t`tUsing Test: $test"
+    Write-Host "`t`tUsing Workspace Config: $workspace_config_out"
+    Write-Host "`t`tUsing Payload: $recipe_payload_dir"
+    Write-Host ""
+
+    # This can also be set via the host configuration json for pythia, however that requires absolute paths.
+    # Resolving the paths allows us to update the version and not have to update the paths.
+    $env:SNPS_VS_VDK_SEARCH_PATHS=(Resolve-Path ${env:REPO_APP_PATH_microsoft.internal.virtualized.kingsgate.svp}\win\release\*\*\*\).toString()
+
+    # Move to the test directory. Pythia will use relative paths once executed, moving into the test
+    # dir will put any any folders that get created there (for things we can't configure atm, ex: SVP output dir).
+    Push-Location -Path $test_results_dir
+
+    & ${env:REPO_APP_PATH_python.win64}\tools\python.exe -m robot `
+    --variable WORKSPACE_CONFIG:"$workspace_config_out" `
+    --variable LOG_DIR:"$test_results_dir" `
+    --variable PAYLOAD_DIR:"$recipe_payload_dir" `
+    --variable HOST_CONFIG_DIR:"$test_results_dir" `
+    --debugfile rlog.txt  `
+    --outputdir $test_results_dir `
+    -K on `
+    -L TRACE `
+    -W 120 `
+    -x Results.xml `
+    $test
+
+    # Move back to wherever the function was invoked from
+    Pop-Location
+
+    Write-Host ""
+    Write-Host "`tPythia Tests Completed. Please see tests results: $test_results_dir"
+    Write-Host ""
+
+    if ($platform -eq "svp") {
+        Write-Host "Removing .svp_simulator dir"
+        Remove-Item $test_results_dir/.svp_simulator -Recurse -Force
+    }
+}
+Write-Host ""
+Write-Host "`tAll $stress_iteration completed successfully."
+Write-Host ""
+}

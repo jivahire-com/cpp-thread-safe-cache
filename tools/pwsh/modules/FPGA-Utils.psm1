85a5fb0de0911c652f467e0aaa111f8c892c4bc6
Aniruddha Tiru <atiru@microsoft.com>
2024-10-10 01:14:29 +0000

    Merged PR 89759: feat: add FPGA utilities to check login, logout, and copy dat files
    ## Why is this change being made? What is changed?
    Some FPGA utilities were required:
    - A command to check if a user is logged in to any FPGA systems and one to log off remotely, without disturbing anyone already using a PC. This is added since there was a report that sometimes users did not log off, and this slowed the systems.
    - A command to copy dat files over to the fpga system for fast flashing
    
    These commands are added in FPGA_Utils.ps1.
    
    ## How is the change being tested?
    
    - [ ] No test, document update only
    - [ ] Unit tests pass
    - [ ] Integration tests pass
    - [x] Manual tests pass (add test specifics): All commands were tested on DH3 and Cloud Devbox.
    - [ ] Others, explain:
    
    ### Test Platform
    
    - [ ] SVP
    - [x] FPGA
    - [ ] Silicon
    - [ ] Others, explain:
    
    ## Notes
    
    ## Risks
    
    ----
    #### AI description  (iteration 1)
    #### PR Classification
    New feature
    
    #### PR Summary
    This pull request adds new utilities for managing FPGA systems, including checking user logins, logging out users, and copying `.dat` files.
    - Added `Check-UserLogin` function to query logged-in users on FPGA systems in `/tools/pwsh/modules/FPGA-Utils.psm1`.
    - Added `Logout-User` function to log out users from FPGA systems in `/tools/pwsh/modules/FPGA-Utils.psm1`.
    - Added `Copy-DatFiles` function to copy `.dat` files to remote PCs in `/tools/pwsh/modules/FPGA-Utils.psm1`.
    - Consolidated system list into a global variable `$Global:SystemList` in `/tools/pwsh/modules/FPGA-Utils.psm1`.
    - Updated aliases to include new functions in `/tools/pwsh/modules/FPGA-Utils.psm1`.
    
    Related work items: #2085828

--- tools/pwsh/modules/FPGA-Utils.psm1
+++ tools/pwsh/modules/FPGA-Utils.psm1
@@ -2,6 +2,28 @@
 # Copyright (C) Microsoft Corporation. All rights reserved.
 #
 
+# Define a global array of hashtables with PC Name and DC-SCM IP
+$Global:SystemList = @(
+    @{ "PC Name" = "DE3";  "DC-SCM IP" = "172.29.232.21" },
+    @{ "PC Name" = "DE4";  "DC-SCM IP" = "172.29.233.220"},
+    @{ "PC Name" = "DE6";  "DC-SCM IP" = "172.29.232.25" },
+    @{ "PC Name" = "DH3";  "DC-SCM IP" = "172.29.232.28" },
+    @{ "PC Name" = "DH4";  "DC-SCM IP" = "172.29.232.22" },
+    @{ "PC Name" = "DH5";  "DC-SCM IP" = "172.29.233.221"},
+    @{ "PC Name" = "DH6";  "DC-SCM IP" = "172.29.232.27" },
+    @{ "PC Name" = "DH7";  "DC-SCM IP" = "172.29.232.24" },
+    @{ "PC Name" = "DH10"; "DC-SCM IP" = "172.29.232.23" },
+    @{ "PC Name" = "DH11"; "DC-SCM IP" = "172.29.232.29" },
+    @{ "PC Name" = "DH12"; "DC-SCM IP" = "172.29.232.13" },
+    @{ "PC Name" = "DH13"; "DC-SCM IP" = "172.29.232.15" },
+    @{ "PC Name" = "DH14"; "DC-SCM IP" = "172.29.233.222"},
+    @{ "PC Name" = "DF2";  "DC-SCM IP" = "172.29.232.11" },
+    @{ "PC Name" = "DF5";  "DC-SCM IP" = "172.29.232.26" },
+    @{ "PC Name" = "DF8";  "DC-SCM IP" = "172.29.232.14" },
+    @{ "PC Name" = "DF11"; "DC-SCM IP" = "172.29.232.12" },
+    @{ "PC Name" = "DF14"; "DC-SCM IP" = "172.29.232.8"  }
+)  
+
 <#
 .SYNOPSIS
 Fetches the DC-SCM IP from a predefines list of PC names and their corresponding DC-SCP IP addresses
@@ -14,30 +36,8 @@ function Get-DCSCPIP {
         [string]$PCName
     )
 
-    # Define an array of hashtables with PC Name and DC-SCM IP
-    $IPList = @(
-        @{ "PC Name" = "DE3";  "DC-SCM IP" = "172.29.232.21" },
-        @{ "PC Name" = "DE4";  "DC-SCM IP" = "172.29.233.220"},
-        @{ "PC Name" = "DE6";  "DC-SCM IP" = "172.29.232.25" },
-        @{ "PC Name" = "DH3";  "DC-SCM IP" = "172.29.232.28" },
-        @{ "PC Name" = "DH4";  "DC-SCM IP" = "172.29.232.22" },
-        @{ "PC Name" = "DH5";  "DC-SCM IP" = "172.29.233.221"},
-        @{ "PC Name" = "DH6";  "DC-SCM IP" = "172.29.232.27" },
-        @{ "PC Name" = "DH7";  "DC-SCM IP" = "172.29.232.24" },
-        @{ "PC Name" = "DH10"; "DC-SCM IP" = "172.29.232.23" },
-        @{ "PC Name" = "DH11"; "DC-SCM IP" = "172.29.232.29" },
-        @{ "PC Name" = "DH12"; "DC-SCM IP" = "172.29.232.13" },
-        @{ "PC Name" = "DH13"; "DC-SCM IP" = "172.29.232.15" },
-        @{ "PC Name" = "DH14"; "DC-SCM IP" = "172.29.233.222"},
-        @{ "PC Name" = "DF2";  "DC-SCM IP" = "172.29.232.11" },
-        @{ "PC Name" = "DF5";  "DC-SCM IP" = "172.29.232.26" },
-        @{ "PC Name" = "DF8";  "DC-SCM IP" = "172.29.232.14" },
-        @{ "PC Name" = "DF11"; "DC-SCM IP" = "172.29.232.12" },
-        @{ "PC Name" = "DF14"; "DC-SCM IP" = "172.29.232.8"  }
-    )  
-
     # Find the matching PC Name and return the DC-SCM IP
-    $index = $IPList | Where-Object { $_."PC Name" -eq $PCName }
+    $index = $SystemList | Where-Object { $_."PC Name" -eq $PCName }
 
     if ($index) {
         return $index."DC-SCM IP"
@@ -117,7 +117,7 @@ Function Write-FPGAFlash(
         Write-Host -ForegroundColor Blue "------------------------------------------------------------------"
         Write-Host -ForegroundColor Blue "Downloading Flash Image via BMC . . ."
         Write-Host -ForegroundColor Blue "------------------------------------------------------------------"
-        Write-Host -ForegroundColor Blue "Ssytem: RDU-120015-$system"
+        Write-Host -ForegroundColor Blue "System: RDU-120015-$system"
         Write-Host -ForegroundColor Blue "IP    : $ip"
         Write-Host -ForegroundColor Blue "File  : $filepath"
         Write-Host -ForegroundColor Blue "Dest  : ${ip}:${dest}"
@@ -140,6 +140,126 @@ Function Write-FPGAFlash(
         Write-Host "SVP/Unknown config detected. Skipping SoC Flash Programming"
         Write-Host -ForegroundColor Blue "------------------------------------------------------------------"
     }
+}
+
+<#
+.SYNOPSIS
+Queries the users logged in on a remote FPGA machine
+
+.EXAMPLE
+Check-UserLogin -user atiru
+Check-UserLogin
+#>
+Function Check-UserLogin(
+    [Parameter(Mandatory=$true)] [string] $user = "all"
+)
+{
+    if ($user -ne "all") {
+        Write-Host "Checking if user $user is logged in on FPGA systems. If this command returns nothing, please check that you are on VPN!!!"
+        Write-Host ""
+        foreach($System in $SystemList) {
+            $PCName = $System."PC Name"
+            Write-Host "Querying user $user on system: RDU-120015-$PCName"
+            query user /SERVER:RDU-120015-$PCName | Select-String -Pattern $user
+        }
+    } else {
+        Write-Host "Checking users logged in on FPGA systems. If this command returns nothing, please check that you are on VPN!!!"
+        Write-Host ""
+        foreach($System in $SystemList) {
+            $PCName = $System."PC Name"
+            Write-Host "Querying users on system: RDU-120015-$PCName"
+            query user /SERVER:RDU-120015-$PCName
+        }
+    }
+    Write-Host ""
+}
+
+<#
+.SYNOPSIS
+Logs out a user from a remote FPGA system.
+
+.EXAMPLE
+Logout-User -pc 'DH6' -user 'atiru'
+#>
+Function Logout-User(
+    [Parameter(Mandatory=$false)] [string] $pc = "all",
+    [Parameter(Mandatory=$true)] [string] $user
+)
+{
+    if ($pc -ne "all") {
+
+        Write-Host -ForegroundColor Blue "------------------------------------------------------------------"
+        Write-Host -ForegroundColor Blue "Logging out user $user from system RDU-120015-$pc"
+        Write-Host -ForegroundColor Blue "------------------------------------------------------------------"
+
+        $sessionId = ((query user /SERVER:RDU-120015-$pc | Where-Object { $_ -match $user }) -split ' +')[2]
+        if ($sessionId -eq $null) {
+            Write-Host -ForegroundColor Red "User $user is not logged in on system RDU-120015-$pc"
+            return
+        }
+
+        logoff $sessionId /SERVER:RDU-120015-$pc
+        Write-Host -ForegroundColor Blue "User $user logged out from system RDU-120015-$pc"
+
+    } else {
+        Write-Host -ForegroundColor Blue "------------------------------------------------------------------"
+        Write-Host -ForegroundColor Blue "Logging out user $user from all systems"
+        Write-Host -ForegroundColor Blue "------------------------------------------------------------------"
+        
+        foreach($System in $SystemList) {
+            $PCName = $System."PC Name"
+            Write-Host -ForegroundColor Blue "Logging out user $user from system RDU-120015-$PCName"
+            $sessionId = ((query user /SERVER:RDU-120015-$PCName | Where-Object { $_ -match $user }) -split ' +')[2]
+            if ($sessionId -eq $null) {
+                Write-Host -ForegroundColor Red "User $user is not logged in on system RDU-120015-$PCName"
+            } else {
+                logoff $sessionId /SERVER:RDU-120015-$PCName
+                Write-Host -ForegroundColor Blue "User $user logged out from system RDU-120015-$PCName"
+            }
+        }
+        
+    }
+    Write-Host -ForegroundColor Blue "------------------------------------------------------------------"
+}
+
+<#
+.SYNOPSIS
+Exports all .dat files from a specified local directory to a remote PC.
+
+.EXAMPLE
+Copy-DatFiles -sourceDir ".build/Debug/arm-eabi-aarch/bin/flash" -destDir "R:/Users/atiru/datfiles" -file "kingsgate.ifwi.fpga.debug.ap.baremetal.dat"
+#>
+Function Copy-DatFiles(
+    [Parameter(Mandatory=$false)] [string] $file = 'all',
+    [Parameter(Mandatory=$false)] [string] $sourceDir = ".build/Debug/arm-eabi-aarch/bin/flash",
+    [Parameter(Mandatory=$true)] [string] $destDir
+)
+{
+    Write-Host -ForegroundColor Blue "------------------------------------------------------------------"
+    Write-Host -ForegroundColor Blue "Copying .dat files to remote PC"
+    Write-Host -ForegroundColor Blue "------------------------------------------------------------------"
+    Write-Host -ForegroundColor Blue "Source Directory      : $sourceDir"
+    Write-Host -ForegroundColor Blue "Destination Directory : $destDir"
+    Write-Host -ForegroundColor Blue "File(s)               : $file"
+    Write-Host -ForegroundColor Blue "------------------------------------------------------------------"
+
+    if ($file -ne 'all')
+    {
+        Write-Host -ForegroundColor Blue "Copying file: $sourceDir/$file"
+        Copy-Item -Path $sourceDir/$file -Destination $destDir -Force
+    } else {
+        $datFiles = Get-ChildItem -Path $sourceDir -Filter *.dat
+        foreach ($datFile in $datFiles)
+        {
+            $filePath = $datFile.FullName
+            Write-Host -ForegroundColor Blue "Copying file: $filePath"
+            Copy-Item -Path $filePath -Destination $destDir -Force
+        }
+    }
+
+    Write-Host -ForegroundColor Blue "------------------------------------------------------------------"
+    Write-Host -ForegroundColor Blue "Completed copying .dat files to remote PC"
+    Write-Host -ForegroundColor Blue "------------------------------------------------------------------"
 }
 
 <#
@@ -167,8 +287,10 @@ Function Get-FPGAHelp()
 }
 
 New-Alias -Name clearscpfw -Value Clear-FPGAFlash -Force
-New-Alias -Name expbins -Value Export-FPGAbins -Force
+New-Alias -Name copydat -Value Copy-DatFiles -Force
 New-Alias -Name flashscpfw -Value Write-FPGAFlash -Force
 New-Alias -Name helpfpga -Value Get-FPGAHelp -Force
+New-Alias -Name checkuser -Value Check-UserLogin -Force
+New-Alias -Name kickuser -Value Logout-User -Force
 
 Export-ModuleMember -Alias * -Function *

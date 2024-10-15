157e11ab074b4b9c13a9c7aad9530845c7ba58f7
Yokesh Maradona Kagoo Bertus Kagoo <v-ybertuskag@microsoft.com>
2024-10-15 23:58:07 +0000

    Merged PR 90094: D2DMBX_Send_Rcv Functional Tests
    ## Why is this change being made?
    Implement test code for D2DMBX Send and Receive functionality
    ## What is changed?
    New files have been added to perform functional tests
    ## How is the change being tested?
    
    - [ ] No test, document update only
    - [ ] Unit tests pass
    - [ ] Integration tests pass
    - [x] Manual tests pass (add test specifics)
    - [ ] Others, explain:
    
    ### Test Platform
    
    - [x] SVP
    - [x] FPGA (Add FPGA Config)
    - [ ] Silicon
    - [ ] Others, explain:
    
    ## Notes
    
    ## Risks
    
    ----
    #### AI description  (iteration 1)
    #### PR Classification
    New feature: Added functional tests for D2D Mailbox ECHO and SEND RECV commands.
    
    #### PR Summary
    This pull request introduces new functional tests for verifying the D2D Mailbox ECHO and SEND RECV commands.
    - Added `d2d_mailbox_cli_test.py` to test the D2D Mailbox ECHO command.
    - Added `d2d_mailbox_cli_test_send_recv.py` to test the D2D Mailbox SEND RECV command.
    - Added `d2d_mailbox_cli_test.robot` to automate the D2D Mailbox CLI echo command test.
    - Added `d2d_mailbox_cli_test_send_recv.robot` to automate the D2D Mailbox CLI send recv command test.
    
    Related work items: #1756074

---
+++ tests/functional/pythia/pylibs/d2dmbx_tests/d2d_mailbox_cli_test_send_recv.py
@@ -0,0 +1,144 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+
+"""
+- Python based Pythia 2.0 Test.
+- Test that checks for D2D Mailbox SEND RECV Command.
+"""
+
+import sys, os
+from pathlib import Path
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'kng_pythia_libs'))
+
+from kng_pythia_test_if import KngPythiaTestIF
+from kng_pythia_test_setup import KngPythiaTestSetup
+
+from pythia.tdk.echofalls.constants.dut_types import DeviceType
+
+from pythia.tdk.echofalls.echofalls_base_test import EchoFallsBaseTest
+
+class d2d_mailbox_cli_test_send_recv(EchoFallsBaseTest):
+
+    """
+    :param name:                Name of the test case
+    :param number:              ADO Number of the test case
+    :param workspace_config:    Path to the workspace config file
+    :param default_log_home:    Path for log storage
+    :param fw_payload_path:     Path to the recipe payload
+    :param dut_platform:        Platform used during the test case execution
+    :param host_config:         Path to the host config file/directory
+    :param host_name:           Name of the host to find the host config file, if host_config is a directory. Defaults to None
+    """
+    def __init__(
+        self,
+        name: str = "D2DMBX_Send_Recv",
+        number: str = "NaN",
+        workspace_config: Path | str = None,
+        default_log_home: str = None,
+        fw_payload_path: str = None,
+        dut_platform: str = "KingsgateSVP",
+        host_config: Path | str = None,
+        host_name: str | None = None,
+    ):
+        
+        # Call parent class init
+        super().__init__(
+            name,
+            number,
+            workspace_config,
+            default_log_home,
+            fw_payload_path,
+            dut_platform,
+            host_config,
+            host_name,
+        )
+    
+    def d2d_mailbox_cli_test_send_recv(self):
+        """ 
+        Test function: 
+            1. Setup the Test. 
+            2. Wait for SCP to be up. Send 'SEND' and 'RECV' command 
+            3. Read response and check if test passed or failed based on response
+            4. Teardown Test. 
+        """
+        self.log.info("Running D2D Mailbox CLI command Test. . .")
+        self.dut.setup()
+
+        core_com_die0_channel=self.dut.mb.node_0.soc.primary_die.scp.channel_manager.get_current_channel()
+        core_com_die1_channel=self.dut.mb.node_0.soc.secondary_die.scp.channel_manager.get_current_channel()
+
+        if (self.dut.get_dut_type() == DeviceType.SVP):
+            # TODO:  Implement changes as per Karls changes
+            # https://dev.azure.com/AzureCSI/Dev/_workitems/edit/2110452 
+            self.log.info("TODO SVP Send Recv Tests")
+            self.dut.teardown()
+            return True
+        
+        self._open_channels(core_com_die0_channel, core_com_die1_channel)
+        
+        try:
+            self.log.info("Waiting for Heartbeat Msg")
+            core_com_die0_channel.read_until(key="ScpHeartBeat", timeout_seconds=500)
+        except Exception as e:
+            self.log.error(f"Error reading self.dut.mb.node_0.soc.primary_die.scp.channel_manager UART: {e}")
+            self.test_notify(step="ScpHeartBeat", msg="Test Fail", _is_error=True)
+            self.dut.teardown()
+            return False
+
+        
+        self._receive_command(core_com_die0_channel, core_com_die1_channel, "icc_d2dmbx")
+        self._receive_command(core_com_die0_channel, core_com_die1_channel, "recv")
+
+        try:
+            self._send_and_receive_commands(core_com_die0_channel, core_com_die1_channel)
+        except Exception as e:
+            core_com_die0_channel.close()
+            core_com_die1_channel.close()
+            self.test_notify(step="D2DMBX SEND RECV Command", msg="Test Fail", is_error=True)
+            self.dut.teardown()
+            return False
+    
+        self._receive_command(core_com_die0_channel, core_com_die1_channel, "recv")
+
+        try:                                 
+            self._send_and_receive_commands(core_com_die1_channel, core_com_die0_channel)
+        except Exception as e:
+            core_com_die0_channel.close()
+            core_com_die1_channel.close()
+            self.test_notify(step="D2DMBX SEND RECV Command", msg="Test Fail", is_error=True)
+            self.dut.teardown()
+            return False
+            
+        core_com_die0_channel.close()
+        core_com_die1_channel.close()
+        self.test_notify(step="D2DMBX SEND RECV Command", msg="Test Done", _is_error=False)
+        self.dut.teardown()
+        return True
+    
+    def _open_channels(self, *channels):
+        for channel in channels:
+            channel.open()
+            assert channel.is_open()
+
+    def _receive_command(self, channel1, channel2, command):
+        self.log.info(f"Submitting command '{command}' on Die0...")
+        channel1.write_line(write_string=command)
+        self.log.info(f"Submitting command '{command}' on Die1...")
+        channel2.write_line(write_string=command)
+
+    def _send_and_receive_commands(self, channel1, channel2):
+        commands = ["send"]
+        for cmd in commands:
+            self._submit_command(channel1, channel2, cmd)
+
+    def _submit_command(self, channel1, channel2, command):
+        self.log.info(f"Submitting command '{command}'...")
+        channel1.write_line(write_string=command)
+        try:
+            channel1.read_until(key="Send Complete: Status[0x0]", timeout_seconds=500)
+            self.log.info("Request SENT Successfully from One Die to Another...")
+            channel2.read_until(key="Recv Complete: Status[0x0]", timeout_seconds=500)
+            self.log.info("Received Response Successfully from One Die to Another...")
+        except Exception as e:
+            self.log.error(f"Error reading SCP UART: {e}")
+            raise
---
+++ tests/functional/pythia/test_suites/d2dmbx_tests/d2d_mailbox_cli_test_send_recv.robot
@@ -0,0 +1,31 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+
+# Guidelines
+# In robot files, all commands and parameters need to be separated by a minimum of 1 tab space. 
+# Pythia returns an error if this is not followed
+
+*** Settings ***
+Documentation    Verifies D2D Mailbox CLI send recv command.
+
+# Import the Python library. Class name must match filename when using file paths.
+# Importing also calls __init__().
+Library     ${CURDIR}/../../pylibs
+Library     pylibs.d2dmbx_tests.d2d_mailbox_cli_test_send_recv
+...         workspace_config=${WORKSPACE_CONFIG}
+...         default_log_home=${LOG_DIR}
+...         fw_payload_path=${PAYLOAD_DIR}
+...         host_config=${HOST_CONFIG_DIR}/hsp_scp_bl_embed_fw_dual_die.json
+...         WITH NAME    d2d_mailbox_cli_test_send_recv_lib
+
+*** Test Cases ***
+Test Case - D2D Mailbox CLI Send Recv Test
+    [Documentation]     Verifies D2D Mailbox CLI Send Recv command.
+
+    # Get an instance of the d2d_mailbox_cli_test_lib library
+    ${test_lib} =       Get Library Instance      d2d_mailbox_cli_test_send_recv_lib
+
+    # Check for both failure and success strings in the output.
+    ${test_result} =        Call Method         ${test_lib}         d2d_mailbox_cli_test_send_recv
+
+    # Validate that parse_log returned test PASS or FAIL
+    Should Be True      ${test_result}

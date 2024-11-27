f8dd0500bafd099f4a65a42dd54d3cc14fe784a7
Yokesh Maradona Kagoo (UST Global Inc) <v-ybertuskag@microsoft.com>
2024-11-27 01:11:48 +0000

    Merged PR 96897: Functional Tests - Config Manager SET CLI command
    ## Why is this change being made?
    To implement functional tests for Config Manager SET CLI command
    ## What is changed?
    Introduced robot tests for validation
    ## How is the change being tested?
    
    - [ ] No test, document update only
    - [ ] Unit tests pass
    - [ ] Integration tests pass
    - [X] Manual tests pass (add test specifics)
    - [ ] Others, explain:
    
    https://artprodeus21.artifacts.visualstudio.com/Adee5775a-8596-4a3a-9408-18bf710b54ba/2610da6a-0168-48f8-b34a-f1c882cb6660/_apis/artifact/cGlwZWxpbmVhcnRpZmFjdDovL0F6dXJlQ1NJL3Byb2plY3RJZC8yNjEwZGE2YS0wMTY4LTQ4ZjgtYjM0YS1mMWM4ODJjYjY2NjAvYnVpbGRJZC84OTAyODEvYXJ0aWZhY3ROYW1lL2Ryb3BfU1ZQX1B5dGhpYV9UZXN0c19TaW5nbGVEaWVfU1ZQX1B5dGhpYV9UZXN0cw2/content?format=file&subPath=%2F2024_11_26_092231%2FConfigmgr_cli_test_set2024-11-26-21_22_32%2FConfigmgr_cli_test_set_debug_2024-11-26-21_22_32.log
    
    ![image.png](https://azurecsi.visualstudio.com/2610da6a-0168-48f8-b34a-f1c882cb6660/_apis/git/repositories/8fc5d0fc-7430-45b1-8963-5d075fba03da/pullRequests/96897/attachments/image.png)
    
    ### Test Platform
    
    - [X] SVP
    - [ ] FPGA (Add FPGA Config)
    - [ ] Silicon
    - [ ] Others, explain:
    
    ## Notes
    
    ## Risks
    
    ----
    #### AI description  (iteration 1)
    #### PR Classification
    New feature
    
    #### PR Summary
    This pull request adds functional tests for the Config Manager SET CLI command.
    - Added `configmgr_set_cli_test.py` to implement the test for the Config Manager SET CLI command.
    - Updated `svp_single_die_ci_test_plan.args` to include the new `configmgr_set_cli_test.robot`.
    - Added `configmgr_set_cli_test.robot` to define the Robot Framework test case for the Config Manager SET CLI command.
    - Updated multiple test plan files (`fpga_weekend_stress_test_plan.args`, `svp_weekend_stress_test_plan.args`, `fpga_nightly_stress_test_plan.args`, `fpga_single_die_test_plan.args`) to include the new `configmgr_set_cli_test.robot`.
    
    Related work items: #2209573

---
+++ tests/functional/pythia/pylibs/configmgr_tests/configmgr_set_cli_test.py
@@ -0,0 +1,167 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+
+"""
+- Python based Pythia 2.0 Test.
+- Test that checks for Config Manager CLI set Command.
+"""
+import time
+import re
+import sys, os
+from pathlib import Path
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'kng_pythia_libs'))
+
+from kng_pythia_test_if import KngPythiaTestIF
+from kng_pythia_test_setup import KngPythiaTestSetup
+
+from pythia.tdk.echofalls.echofalls_base_test import EchoFallsBaseTest
+
+class configmgr_set_cli_test(EchoFallsBaseTest):
+
+    """
+    :param name:                Name of the test case
+    :param number:              ADO Number of the test case
+    :param workspace_config:    Path to the workspace config file
+    :param default_log_home:    Path for log storage
+    :param fw_payload_path:     Path to the recipe payload
+    :param dut_platform:        Platform used during the test case execution
+    :param host_config:         Path to the host config file/directory
+    :param host_name:           Name of the host to find the host config file, if host_config is a directory. Defaults to None
+    """
+    def __init__(
+        self,
+        name: str = "Configmgr_cli_test_set",
+        number: str = "NaN",
+        workspace_config: Path | str = None,
+        default_log_home: str = None,
+        fw_payload_path: str = None,
+        dut_platform: str = "KingsgateSVP",
+        host_config: Path | str = None,
+        host_name: str | None = None,
+    ):
+        
+        # Call parent class init
+        super().__init__(
+            name,
+            number,
+            workspace_config,
+            default_log_home,
+            fw_payload_path,
+            dut_platform,
+            host_config,
+            host_name,
+        )
+    
+    def configmgr_set_cli_test(self):
+        """ 
+        Test function: 
+            1. Setup the Test. 
+            2. Wait for SCP to be up. Send 'set' command 
+            3. Read response and check if test passed or failed based on response
+            4. Teardown Test. 
+        """
+        self.log.info("Running Config Manager CLI command Test. . .")
+        self.dut.setup()
+
+        core_com_channel=self.dut.mb.node_0.soc.primary_die.scp.channel_manager.get_current_channel()
+        core_com_channel.open()
+        assert core_com_channel.is_open()
+        
+        try:
+            self.log.info("Waiting for Heartbeat Msg")
+            core_com_channel.read_until(key="ScpHeartBeat", timeout_seconds=900)
+        except Exception as e:
+            self.log.error(f"Error reading self.dut.mb.node_0.soc.primary_die.scp.channel_manager UART: {e}")
+            self.test_notify(step="ScpHeartBeat", msg="Test Fail", _is_error=True)
+            self.dut.teardown()
+            return False
+
+        self.log.info("Submitting Config Manager set command . . .")
+        command="cfg_mgr"
+        core_com_channel.write_line(write_string=command)
+
+        command="cfg_mgr_set 8 5"
+        self.log.info(f"Submitting SET command {command} . . .") 
+        core_com_channel.write_line(write_string=command)
+
+        try:
+            data = core_com_channel.read_until(key="Knob configuration update completed", timeout_seconds=300)
+            self.log.info("SET command executed Successfully . . .")
+            set_data = self.validate_data(data)
+            self.log.info(f"SET Value: {set_data}\n")
+            if set_data is None:
+                self.log.info("Data for SET is NOT SUCCESSFUL")
+                core_com_channel.close()
+                self.test_notify(step="Config Manager SET Command", msg="Test Fail", _is_error=True)
+                self.dut.teardown()
+                return False
+        except Exception as e:
+            self.log.error(f"Error reading SCP UART: {e}")
+            core_com_channel.close()
+            self.test_notify(step="Config Manager SET Command", msg="Test Fail", _is_error=True)
+            self.dut.teardown()
+            return False
+        
+        command="cfg_mgr_dump 8"
+        self.log.info(f"Submitting DUMP command {command} . . .") 
+        core_com_channel.write_line(write_string=command)
+
+        try:
+            core_com_channel.read_until(key="Ok", timeout_seconds=300)
+            check = core_com_channel.read_until(key="Ok", timeout_seconds=300)
+            self.log.info("DUMP command executed successfully . . .")
+            # Store GUID after issuing GET command and validate it with GUID on SET
+            get_data = self.validate_data(check)
+            self.log.info(f"GET Value: {get_data}\n")
+            if get_data is None:
+                self.log.info("Data for Get is NOT SUCCESSFUL")
+                core_com_channel.close()
+                self.test_notify(step="Config Manager SET Command", msg="Test Fail", _is_error=True)
+                self.dut.teardown()
+                return False
+            if get_data != set_data:
+                self.log.info("Data byte for Set and GET validation is NOT SUCCESSFUL")
+                core_com_channel.close()
+                self.test_notify(step="Config Manager SET Command", msg="Test Fail", _is_error=True)
+                self.dut.teardown()
+                return False
+            self.log.info("SET-DUMP Data byte validation is successful . . .")
+        except Exception as e:
+            self.log.error(f"Error reading SCP UART: {e}")
+            core_com_channel.close()
+            self.test_notify(step="Config Manager SET Command", msg="Test Fail", _is_error=True)
+            self.dut.teardown()
+            return False
+
+        core_com_channel.close()
+        self.test_notify(step="Config Manager SET Command", msg="Test Done", _is_error=False)
+        self.dut.teardown()
+
+        return True
+    
+
+    #Parse data bytes information from SET message
+    def validate_data(self, data):
+        last_value = None
+        lines = data.splitlines()
+        for i, line in enumerate(lines):
+            if "Primitive Data If Possible" in line:
+                # Check the next line for the value
+                if i + 1 < len(lines):
+                    next_line = lines[i + 1].strip()
+                    # Extract digits from the next line
+                    last_value = ''.join(filter(str.isdigit, next_line))
+                    self.log.info(f"Digit found: {last_value}")
+        
+        if last_value:
+            self.log.info(f"Data Byte found: {last_value}")
+            return last_value
+        else:
+            self.log.info("Data Byte NOT found . . .")
+            return None
+
+
+
+
+
+

a517952f7d07af6f47fd14016d54253fbd2397a8
Yokesh Maradona Kagoo (UST Global Inc) <v-ybertuskag@microsoft.com>
2024-12-04 01:37:31 +0000

    Merged PR 96523: Functional Test - Die2Die - Echo Command - Server ASYNC and Client SYNC
    ## Why is this change being made?
    To implement functional tests for D2D echo command with server ASYNC and client SYNC
    ## What is changed?
    Implemented robot and pythia code for this tests
    ## How is the change being tested?
    
    - [ ] No test, document update only
    - [ ] Unit tests pass
    - [ ] Integration tests pass
    - [X] Manual tests pass (add test specifics)
    - [ ] Others, explain:
    https://azurecsi.visualstudio.com/Woodinville/_build/results?buildId=885656&view=artifacts&pathAsName=false&type=publishedArtifacts
    
    ![image (2).png](https://azurecsi.visualstudio.com/2610da6a-0168-48f8-b34a-f1c882cb6660/_apis/git/repositories/8fc5d0fc-7430-45b1-8963-5d075fba03da/pullRequests/96523/attachments/image%20%282%29.png)
    
    ### Test Platform
    
    - [X] SVP
    - [ ] FPGA (Add FPGA Config)
    - [ ] Silicon
    - [ ] Others, explain:
    
    ## Notes
    
    ## Risks
    
    ----
    #### AI description  (iteration 1)
    #### PR Classification
    New feature
    
    #### PR Summary
    This pull request adds a new functional test for the D2D Mailbox Echo Command with Server ASYNC and Client SYNC configuration.
    - Added `d2d_mailbox_cli_test_echo_serverasync_clientsync.py` to implement the new test.
    - Added `d2d_mailbox_cli_test_echo_serverasync_clientsync.robot` to define the test case in the robot framework.
    - Updated `svp_dual_die_ci_test_plan.args` and `fpga_dual_die_test_plan.args` to include the new test in the test plans.
    
    Related work items: #2133530

---
+++ tests/functional/pythia/pylibs/d2dmbx_tests/d2d_mailbox_cli_test_echo_serverasync_clientsync.py
@@ -0,0 +1,154 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+
+"""
+- Python based Pythia 2.0 Test.
+- Test that checks for D2D Mailbox Echo Command ServerASYNC-ClientSYNC.
+"""
+
+import sys, os
+from pathlib import Path
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'kng_pythia_libs'))
+
+from kng_pythia_test_if import KngPythiaTestIF
+from kng_pythia_test_setup import KngPythiaTestSetup
+
+from pythia.tdk.echofalls.constants.dut_types import DeviceType
+
+from pythia.tdk.echofalls.echofalls_base_test import EchoFallsBaseTest
+
+class d2d_mailbox_cli_test_echo_serverasync_clientsync(EchoFallsBaseTest):
+
+    """
+    :param name:                Name of the test case
+    :param number:              ADO Number of the test case
+    :param workspace_config:    Path to the workspace config file
+    :param default_log_home:    Path for log storage
+    :param fw_payload_path:     Path to the recipe payload
+    :param dut_platform:        Platform used during the test case execution
+    :param host_config:         Path to the host config file/directory
+    :param host_name:           Name of the host to find the host config file, if host_config is a directory. Defaults to None
+    """
+    def __init__(
+        self,
+        name: str = "D2DMBX_Echo_ServerASYNC_ClientSYNC",
+        number: str = "NaN",
+        workspace_config: Path | str = None,
+        default_log_home: str = None,
+        fw_payload_path: str = None,
+        dut_platform: str = "KingsgateSVP",
+        host_config: Path | str = None,
+        host_name: str | None = None,
+    ):
+        
+        # Call parent class init
+        super().__init__(
+            name,
+            number,
+            workspace_config,
+            default_log_home,
+            fw_payload_path,
+            dut_platform,
+            host_config,
+            host_name,
+        )
+    
+    def d2d_mailbox_cli_test_echo_serverasync_clientsync(self):
+        """ 
+        Test function: 
+            1. Setup the Test. 
+            2. Wait for SCP to be up. Send 'ECHO' command 
+            3. Read response and check if test passed or failed based on response
+            4. Teardown Test. 
+        """
+        self.log.info("Running D2D Mailbox CLI Echo command Test. . .")
+        self.dut.setup()
+
+        core_com_die0_channel=self.dut.mb.node_0.soc.primary_die.scp.channel_manager.get_current_channel()
+        core_com_die1_channel=self.dut.mb.node_0.soc.secondary_die.scp.channel_manager.get_current_channel()
+        
+        self._open_channels(core_com_die0_channel, core_com_die1_channel)
+        
+        try:
+            self.log.info("Waiting for Heartbeat Msg")
+            core_com_die0_channel.read_until(key="ScpHeartBeat", timeout_seconds=900)
+        except Exception as e:
+            self.log.error(f"Error reading self.dut.mb.node_0.soc.primary_die.scp.channel_manager UART: {e}")
+            self.test_notify(step="ScpHeartBeat", msg="Test Fail", _is_error=True)
+            self.dut.teardown()
+            return False
+
+        try:
+            self.log.info("Waiting for Heartbeat Msg")
+            core_com_die1_channel.read_until(key="ScpHeartBeat", timeout_seconds=900)
+        except Exception as e:
+            self.log.error(f"Error reading self.dut.mb.node_0.soc.primary_die.scp.channel_manager UART: {e}")
+            self.test_notify(step="ScpHeartBeat", msg="Test Fail", _is_error=True)
+            self.dut.teardown()
+            return False
+
+        
+        self._receive_command(core_com_die0_channel, core_com_die1_channel, "icc_d2dmbx")
+
+        try:
+            self._server_command(core_com_die0_channel)
+            self._client_command(core_com_die1_channel)
+            self.log.info("Die 0 to Die 1 ECHO Complete")
+        except Exception as e:
+            core_com_die0_channel.close()
+            core_com_die1_channel.close()
+            self.test_notify(step="D2DMBX ECHO Command", msg="Test Fail", _is_error=True)
+            self.dut.teardown()
+            return False
+
+        try:
+            self._server_command(core_com_die1_channel)
+            self._client_command(core_com_die0_channel)
+            self.log.info("Die 1 to Die 0 ECHO Complete")
+        except Exception as e:
+            core_com_die0_channel.close()
+            core_com_die1_channel.close()
+            self.test_notify(step="D2DMBX ECHO Command", msg="Test Fail", _is_error=True)
+            self.dut.teardown()
+            return False
+            
+        core_com_die0_channel.close()
+        core_com_die1_channel.close()
+        self.test_notify(step="D2DMBX ECHO Command", msg="Test Done", _is_error=False)
+        self.dut.teardown()
+        return True
+    
+    def _open_channels(self, *channels):
+        for channel in channels:
+            channel.open()
+            assert channel.is_open()
+
+    def _receive_command(self, channel1, channel2, command):
+        self.log.info(f"Submitting command '{command}' on Die0...")
+        channel1.write_line(write_string=command)
+        self.log.info(f"Submitting command '{command}' on Die1...")
+        channel2.write_line(write_string=command)
+
+    def _server_command(self, channel1):
+        commands = ["echo_serv"]
+        for cmd in commands:
+            channel1.write_line(write_string=cmd)
+            try:
+                self.log.info("Reading server message")
+                channel1.read_until(key="Recv Initiated: Status[0x0]", timeout_seconds=300)
+                self.log.info("Request SENT Successfully from One Die to Another...")
+            except Exception as e:
+                self.log.error(f"Error reading SCP UART: {e}")
+                raise
+
+    def _client_command(self, channel2):
+        commands = ["echo_client_sync"]
+        for cmd in commands:
+            channel2.write_line(write_string=cmd)
+            try:
+                self.log.info("Reading client message")
+                channel2.read_until(key="Echo Test Passed", timeout_seconds=300)
+                self.log.info("Request SENT Successfully from One Die to Another...")
+            except Exception as e:
+                self.log.error(f"Error reading SCP UART: {e}")
+                raise

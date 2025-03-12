40c5fd31ce493d6feaf7bf10ae1ae1199718e20c
Sebastian Kao (PHOENIX TECHNOLOGIES Inc) <v-skao@microsoft.com>
2025-03-12 12:41:37 +0000

    Merged PR 109574: crash dump integration test
    ## Why is this change being made?
    add crash dump test
    ## What is changed?
    
    ## How is the change being tested?
    
    - [ ] No test, document update only
    - [ ] Unit tests pass
    - [ ] Integration tests pass
    - [X] Manual tests pass (add test specifics)
    - [ ] Others, explain:
    
    ### Test Platform
    
    - [X] SVP
    - [ ] FPGA (Add FPGA Config)
    - [ ] Silicon
    - [ ] Others, explain:
    
    ## Notes
    
    ## Risks
    
    update crash dump test
    ![image.png](https://azurecsi.visualstudio.com/2610da6a-0168-48f8-b34a-f1c882cb6660/_apis/git/repositories/8fc5d0fc-7430-45b1-8963-5d075fba03da/pullRequests/109574/attachments/image.png)
    
    ----
    #### AI description  (iteration 1)
    #### PR Classification
    Integration test for crash dump CLI commands.
    
    #### PR Summary
    This pull request adds integration tests for various crash dump CLI commands.
    - Added `crash_dump_set_single_core_mode_test.py` to test the crash dump set single core mode functionality.
    - Added `crash_dump_stack_overflow_test.py` to test the crash dump stack overflow functionality.
    - Added `crash_dump_trigger_exception_test.py` to test the crash dump trigger exception functionality.
    - Added corresponding Robot Framework test files for each of the above Python test scripts.
    <!-- GitOpsUserAgent=GitOps.Apps.Server.pullrequestcopilot -->
    
    Related work items: #1484999

---
+++ tests/functional/pythia/pylibs/crash_dump_tests/crash_dump_trigger_exception_test.py
@@ -0,0 +1,110 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+
+"""
+- Python based Pythia 2.0 Test.
+- Tests Crash Dump Die0.
+"""
+import time
+import sys, os
+from pathlib import Path
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'kng_pythia_libs'))
+
+from kng_pythia_test_if import KngPythiaTestIF
+from kng_pythia_test_setup import KngPythiaTestSetup
+from pythia.tdk.echofalls.constants.dut_types import DeviceType
+from pythia.tdk.echofalls.echofalls_base_test import EchoFallsBaseTest
+import re
+
+# Class name must match file name for Robot Framework Library usage
+class crash_dump_trigger_exception_test(EchoFallsBaseTest):
+
+    """
+    :param name:                Name of the test case
+    :param number:              ADO Number of the test case
+    :param workspace_config:    Path to the workspace config file
+    :param default_log_home:    Path for log storage
+    :param fw_payload_path:     Path to the recipe payload
+    :param dut_platform:        Platform used during the test case execution
+    :param host_config:         Path to the host config file/directory
+    :param host_name:           Name of the host to find the host config file, if host_config is a directory. Defaults to None
+    """
+    def __init__(
+        self,
+        name: str = "crash_dump_test",
+        number: str = "NaN",
+        workspace_config: Path | str = None,
+        default_log_home: str = None,
+        fw_payload_path: str = None,
+        dut_platform: str = "KingsgateSVP",
+        host_config: Path | str = None,
+        host_name: str | None = None,
+    ):
+        
+        # Call parent class init
+        super().__init__(
+            name,
+            number,
+            workspace_config,
+            default_log_home,
+            fw_payload_path,
+            dut_platform,
+            host_config,
+            host_name,
+        )
+    
+    def crash_dump_trigger_exception_test(self):
+        """
+        Crash Dump test:
+            1. Setup the Test.
+            2. Executes Crash Dump cli command.
+            3. Teardown Test.
+        """
+        self.log.info("Running Crash Dump tests on Die0  . . .")
+        self.dut.setup()
+
+        if self.dut.get_dut_type() == DeviceType.BIGFPGA:
+            self.log.warning("Device type is bigFPGA. Performing an additional OOB reset ...")
+            KngPythiaTestSetup.fpga_oob_reset(self.log)
+            
+        scp_channel=self.dut.mb.node_0.soc.primary_die.scp.channel_manager.get_current_channel()
+        # Open SCP channel
+        scp_channel.open()
+        if not scp_channel.is_open():
+            scp_channel.close()
+            self.test_notify(step="Open_Channels", msg="Test Fail", _is_error=True)
+            self.dut.teardown()
+            time.sleep(30)
+            return False
+
+        try:
+            # Wait for SOS Boot Completion message and enter commands
+            self.log.info("Waiting for SOS BOOT  Msg")
+            scp_channel.read_until(key="SOS boot completed", timeout_seconds=1800)
+        except Exception as e:
+            self.log.error(f"Error reading self.dut.mb.node_0.soc.primary_die.scp.channel_manager UART: {e}")
+            self.test_notify(step="ScpHeartBeat", msg="Test Fail", _is_error=True)
+            self.dut.teardown()
+            time.sleep(30)
+            return False
+
+
+        command = "crashdump cd_trigger_exception"
+        self.log.info(f"Submitting {command}\n")
+        scp_channel.write_line(write_string=command)
+        try:
+            scp_channel.read_until(key="BugCheck: 0x80380005", timeout_seconds=30)
+        except Exception as e:
+            self.log.error(f"Error Crash Dump. Die0: {e}")
+            self.test_notify(step="Crash Dump.", msg="Test Fail", _is_error=True)
+            scp_channel.close()
+            self.dut.teardown()
+            time.sleep(30)
+            return False 
+
+        # Close connection to SCP
+        self.test_notify(step="Crash Dump Die0", msg="Test Done", _is_error=False)
+        scp_channel.close()
+        self.dut.teardown()
+        time.sleep(30)
+        return True

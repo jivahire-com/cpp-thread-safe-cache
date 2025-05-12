3c4b5928001970390344beff10b71b711c937240
Maurice Ma <maurice.ma@microsoft.com>
2025-05-12 23:58:52 +0000

    Merged PR 119625: Enable DDRSS debug message
    ## Why is this change being made?
    Compile in necessary DDR debug message in preparation for SBU.
    
    ## What is changed?
    Removed the "-DDDRSS_COMPILE_DEBUG_LEVEL=DDRSS_DEBUG_LEVEL_CRITICAL" compiling option for SCP image build and added knobs to control the debug level print.
    
    ## How is the change being tested?
    
    - [ ] No test, document update only
    - [x] Unit tests pass
    - [ ] Integration tests pass
    - [x] Manual tests pass (add test specifics)
    - [ ] Others, explain:
    
    ### Test Platform
    
    - [x] SVP
    - [ ] FPGA (Add FPGA Config)
    - [ ] Silicon
    - [ ] Others, explain:
    
    ## Notes
    
    ## Risks
    
    Enable DDRSS debug message in preparation for SBU.  By enabling more debug print, it increased the .text size by 7KB and .rodata size by 5KB.
    
    ----
    #### AI description  (iteration 1)
    #### PR Classification
    This pull request implements a new debug configuration feature for DDRSS.
    
    #### PR Summary
    The pull request enables dynamic control over DDRSS debug messaging and parallel PHY training by introducing new configuration knobs and updating initialization logic.
    - `config/knobs_config/kng_mscp_ddrss_config_knobs.xml`: Added knobs `ddr_debug_level` and `parallel_phy_train_en` to control the DDR debug level and PHY training behavior.
    - `src/libs/ddrss/ddrss_library/src/ddrss.c`: Modified initialization to retrieve debug level and parallel PHY training settings from configuration.
    - `src/externs/silibs/libraries/silibs/CMakeLists.txt`: Removed the hard-coded compile-time debug level definition.
    <!-- GitOpsUserAgent=GitOps.Apps.Server.pullrequestcopilot -->
    
    Related work items: #2609381

--- src/libs/ddrss/ddrss_library/src/ddrss.c
+++ src/libs/ddrss/ddrss_library/src/ddrss.c
@@ -97,7 +97,8 @@ void prod_ddrss_lib_init(KNG_DIE_ID die_num)
 
     // Update DDRSS cfgs to match actual platform cfg
     ddrss_cfgs.die_id = (DIE_INSTANCE)die_num;
-    ddrss_cfgs.debug_level = DDRSS_DEBUG_LEVEL_INFO;
+    ddrss_cfgs.debug_level = config_get_ddr_debug_level();
+    ddrss_cfgs.parallel_phy_train_en = config_get_parallel_phy_train_en();
 
     ddrss_cfgs.ext_knobs.addr_err_report = config_get_addr_err_report();
     ddrss_cfgs.ext_knobs.address_hashing_mode = config_get_address_hashing_mode();

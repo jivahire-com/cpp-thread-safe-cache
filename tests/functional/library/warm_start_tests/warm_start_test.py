8081c30f64d6a2370ae522ab54aa5ba575806a87
Yokesh Maradona Kagoo (UST Global Inc) <v-ybertuskag@microsoft.com>
2025-10-24 06:03:52 +0000

    Merged PR 150181: Add CMD available log validation on SoC warm reset function test
    ## Why is this change being made?
    To fix issue with warm reset function test failure
    ## What is changed?
    Added CMD available log validation on the test
    ## How is the change being tested?
    
    - [ ] No test, document update only
    - [ ] Unit tests pass
    - [ ] Integration tests pass
    - [X] Manual tests pass (add test specifics)
    - [ ] Others, explain:
    
    ![image.png](https://azurecsi.visualstudio.com/2610da6a-0168-48f8-b34a-f1c882cb6660/_apis/git/repositories/8fc5d0fc-7430-45b1-8963-5d075fba03da/pullRequests/150181/attachments/image.png)
    
    ### Test Platform
    
    - [ ] SVP
    - [ ] FPGA (Add FPGA Config)
    - [X] Silicon
    - [ ] Others, explain:
    
    ## Notes
    
    ## Risks

--- tests/functional/library/warm_start_tests/warm_start_test.py
+++ tests/functional/library/warm_start_tests/warm_start_test.py
@@ -93,6 +93,7 @@ class warm_start_test(EchoFallsBaseTest):
             self.log.info("Waiting for Windows Boot")
             # Wait till windows boot as scp crashes during warm reset before windows is booted-Bug3063669
             apns_connection.read_until(key="SAC>", timeout_seconds=900)
+            apns_connection.read_until(key="CMD command is now available", timeout_seconds=250)
             command="\r\n"
             hsp_connection.write_line(write_string=command)
             command="reset 4 2 0x9A 1"
